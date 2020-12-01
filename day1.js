72e488ef78ea1cbbf9c26f4096074bd91ce7219e
Olli Helminen <opadele@gmail.com>
2020-12-01 21:51:47 +0200

    Part 2 solution

--- day1.js
+++ day1.js
@@ -9,12 +9,23 @@ var instance = axios_1["default"].create({
 instance.get('/1/input', { headers: {
         Cookie: "session=" + config.session + ";"
     } })
-    .then(function (res) { return solve(res.data); })["catch"](function (err) { return console.log(err); });
-function solve(input) {
-    var input_array = input.split('\n');
+    .then(function (res) { solve_part1(res.data.split('\n')); solve_part2(res.data.split('\n')); })["catch"](function (err) { return console.log(err); });
+function solve_part1(input_array) {
     for (var _i = 0, input_array_1 = input_array; _i < input_array_1.length; _i++) {
         var cell = input_array_1[_i];
         if (input_array.includes(String(2020 - Number(cell))))
             console.log(Number(cell) * (2020 - Number(cell)));
+    }
+}
+function solve_part2(input_array) {
+    for (var _i = 0, input_array_2 = input_array; _i < input_array_2.length; _i++) {
+        var cell1 = input_array_2[_i];
+        var intermediate = 2020 - Number(cell1);
+        for (var _a = 0, input_array_3 = input_array; _a < input_array_3.length; _a++) {
+            var cell2 = input_array_3[_a];
+            var number_to_find = intermediate - Number(cell2);
+            if (input_array.includes(String(number_to_find)))
+                console.log(Number(cell1) * Number(cell2) * number_to_find);
+        }
     }
 }
--- day1.ts
+++ day1.ts
@@ -9,12 +9,21 @@ const instance = axios.create({
 instance.get('/1/input', {headers:{
     Cookie: `session=${config.session};`
 }})
-.then(res => solve(res.data))
+.then(res => {solve_part1(res.data.split('\n')); solve_part2(res.data.split('\n'))})
 .catch(err => console.log(err))
 
-function solve(input:string){
-    let input_array:Array<string> = input.split('\n');
+function solve_part1(input_array:Array<string>){
     for (let cell of input_array){
         if (input_array.includes(String(2020-Number(cell)))) console.log(Number(cell)*(2020-Number(cell)));
+    }
+}
+
+function solve_part2(input_array:Array<string>){
+    for(let cell1 of input_array){
+        let intermediate = 2020 - Number(cell1);
+        for (let cell2 of input_array){
+            let number_to_find = intermediate - Number(cell2);
+            if(input_array.includes(String(number_to_find))) console.log(Number(cell1)*Number(cell2)*number_to_find);
+        }
     }
 }
